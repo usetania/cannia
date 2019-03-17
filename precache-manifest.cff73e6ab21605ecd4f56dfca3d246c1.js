f81f1cfed36c11b6b3771cc40c5efa20ee2ddd3d
Asep Bagja Priandana <bepitulaz@gmail.com>
2019-03-17 16:30:31 +0800

    Updates

---
+++ precache-manifest.cff73e6ab21605ecd4f56dfca3d246c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cf0d15cd214fb28d85e8",
+    "url": "/static/css/main.fdad8a6e.chunk.css"
+  },
+  {
+    "revision": "cf0d15cd214fb28d85e8",
+    "url": "/static/js/main.0524a18c.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "a6fe2b2132054de9cd01",
+    "url": "/static/js/2.56648e6b.chunk.js"
+  },
+  {
+    "revision": "669b19bcac008be7fbdc1ffbdb3d42cb",
+    "url": "/static/media/logo_long.669b19bc.svg"
+  },
+  {
+    "revision": "c923f3802387d4c7f5091083a28cf599",
+    "url": "/index.html"
+  }
+];
